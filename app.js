9d921328afe8fe76c3d102b1ce4414ff2f134b25
Disa0808 <levshukdenis@mail.ru>
2019-01-05 20:14:33 +0300

    feat: Change inline html to html files

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -7,9 +9,12 @@ const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded());
 
-app.use(adminRoutes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
 
 app.listen(3000, () => {
   console.log(`Server started on port`);
