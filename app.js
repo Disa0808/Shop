a35c3ca0cb0b865a2c201dd7d52aa5410b30ee03
Disa0808 <levshukdenis@mail.ru>
2019-01-05 19:23:25 +0300

    feat: Inital commit. Create a basic structure of project

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded());
+
+app.use(adminRoutes);
+app.use(shopRoutes);
+
+
+app.listen(3000, () => {
+  console.log(`Server started on port`);
+});
